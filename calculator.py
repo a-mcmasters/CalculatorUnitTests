56ab9191ccfda0249058585b83d7bbca45d40f29
a-mcmasters <36310514+a-mcmasters@users.noreply.github.com>
2023-06-05 14:22:09 -0400

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -12,7 +12,7 @@ class Calculator:
 
     def divide(self, a, b):
         if b == 0:
-            raise ValueError("Cannot divide by zero!")
+            raise ValueError("Cannot divide by zero")
         return a / b
     
     def power(self, a, b):
@@ -20,10 +20,45 @@ class Calculator:
 
     def sqrt(self, a):
         if a < 0:
-            return None
+            raise ValueError("Cannot take the square root of a negative number")
         return math.sqrt(a)
 
     def factorial(self, a):
-        if a < 0:
-            return None
+        if not isinstance(a, int) or a < 0:
+            raise ValueError("Factorial is only defined for non-negative integers")
         return math.factorial(a)
+    
+    def sin(self, a):
+        return math.sin(a)
+
+    def cos(self, a):
+        return math.cos(a)
+
+    def tan(self, a):
+        return math.tan(a)
+
+    def asin(self, a):
+        if not -1 <= a <= 1:
+            raise ValueError("Invalid input to asin: input must be in the range [-1, 1]")
+        return math.asin(a)
+
+    def acos(self, a):
+        if not -1 <= a <= 1:
+            raise ValueError("Invalid input to acos: input must be in the range [-1, 1]")
+        return math.acos(a)
+
+    def atan(self, a):
+        return math.atan(a)
+
+    def log(self, a):
+        if a <= 0:
+            raise ValueError("Cannot take the logarithm of a non-positive number")
+        return math.log(a)
+
+    def log10(self, a):
+        if a <= 0:
+            raise ValueError("Cannot take the logarithm of a non-positive number")
+        return math.log10(a)
+
+    def exp(self, a):
+        return math.exp(a)
--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, math
 from calculator import Calculator
 
 class TestCalculator(unittest.TestCase):
@@ -9,22 +9,30 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(2, 2), 4)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(-1, -1), -2)
+        self.assertEqual(self.calc.add(123456, 543210), 666666)
+        self.assertEqual(self.calc.add(0, 0), 0)
 
     def test_subtract(self):
-        self.assertEqual(self.calc.subtract(2, 2), 0)
+        self.assertEqual(self.calc.subtract(1, 1), 0)
         self.assertEqual(self.calc.subtract(-1, 1), -2)
         self.assertEqual(self.calc.subtract(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
 
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(2, 2), 4)
         self.assertEqual(self.calc.multiply(-1, 1), -1)
         self.assertEqual(self.calc.multiply(-1, -1), 1)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+        self.assertEqual(self.calc.multiply(0.1, 100), 10)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(2, 2), 1)
         self.assertEqual(self.calc.divide(-1, 1), -1)
         self.assertEqual(self.calc.divide(-1, -1), 1)
-
+        self.assertEqual(self.calc.divide(0, 100), 0)
+        self.assertEqual(self.calc.divide(2, -2), -1)
+        self.assertEqual(self.calc.divide(2, -0.2), -10)
+  
         with self.assertRaises(ValueError):
             self.calc.divide(10, 0)
 
@@ -32,16 +40,94 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.power(2, 3), 8)
         self.assertEqual(self.calc.power(-2, 3), -8)
         self.assertEqual(self.calc.power(-2, 0), 1)
+        self.assertEqual(self.calc.power(0, 10), 0)
 
     def test_sqrt(self):
         self.assertEqual(self.calc.sqrt(4), 2)
         self.assertEqual(self.calc.sqrt(9), 3)
-        self.assertEqual(self.calc.sqrt(-1), None)
+        self.assertEqual(self.calc.sqrt(0), 0)
+        self.assertEqual(self.calc.sqrt(1), 1)
+ 
+        with self.assertRaises(ValueError):
+            self.calc.sqrt(-1)
 
     def test_factorial(self):
         self.assertEqual(self.calc.factorial(5), 120)
         self.assertEqual(self.calc.factorial(0), 1)
-        self.assertEqual(self.calc.factorial(-10), None)
+
+        with self.assertRaises(ValueError):
+            self.calc.factorial(-1)
+        with self.assertRaises(ValueError):
+            self.calc.factorial(3.5)
+
+    def test_sin(self):
+        self.assertAlmostEqual(self.calc.sin(0), 0)
+        self.assertAlmostEqual(self.calc.sin(math.pi/2), 1)
+        self.assertAlmostEqual(self.calc.sin(math.pi), 0)
+
+    def test_cos(self):
+        self.assertAlmostEqual(self.calc.cos(0), 1)
+        self.assertAlmostEqual(self.calc.cos(math.pi/2), 0)
+        self.assertAlmostEqual(self.calc.cos(math.pi), -1)
+        self.assertAlmostEqual(self.calc.cos(-math.pi/2), 0)
+
+    def test_tan(self):
+        self.assertAlmostEqual(self.calc.tan(0), 0)
+        self.assertAlmostEqual(self.calc.tan(math.pi/4), 1)
+        self.assertAlmostEqual(self.calc.tan(math.pi), 0)
+        self.assertAlmostEqual(self.calc.tan(-math.pi/4), -1)
+
+    def test_asin(self):
+        self.assertAlmostEqual(self.calc.asin(0), 0)
+        self.assertAlmostEqual(self.calc.asin(1), math.pi/2)
+        self.assertAlmostEqual(self.calc.asin(-1), -math.pi/2)
+        self.assertAlmostEqual(self.calc.asin(0.5), math.pi/6)
+
+        with self.assertRaises(ValueError):
+            self.calc.asin(-1.5)
+        with self.assertRaises(ValueError):
+            self.calc.asin(2)
+
+    def test_acos(self):
+        self.assertAlmostEqual(self.calc.acos(1), 0)
+        self.assertAlmostEqual(self.calc.acos(0), math.pi/2)
+        self.assertAlmostEqual(self.calc.acos(-1), math.pi)
+        self.assertAlmostEqual(self.calc.acos(0.5), math.pi/3)
+
+        with self.assertRaises(ValueError):
+            self.calc.acos(-1.5)
+        with self.assertRaises(ValueError):
+            self.calc.acos(2)
+
+    def test_atan(self):
+        self.assertAlmostEqual(self.calc.atan(0), 0)
+        self.assertAlmostEqual(self.calc.atan(1), math.pi/4)
+        self.assertAlmostEqual(self.calc.atan(-1), -math.pi/4)
+        self.assertAlmostEqual(self.calc.atan(0.5), math.atan(0.5))
+
+    def test_log(self):
+        self.assertAlmostEqual(self.calc.log(1), 0)
+        self.assertAlmostEqual(self.calc.log(math.e), 1)
+        
+        with self.assertRaises(ValueError):
+            self.calc.log(0)
+        with self.assertRaises(ValueError):
+            self.calc.log(-1)
+
+    def test_log10(self):
+        self.assertAlmostEqual(self.calc.log10(1), 0)
+        self.assertAlmostEqual(self.calc.log10(10), 1)
+        
+        with self.assertRaises(ValueError):
+            self.calc.log10(0)
+        with self.assertRaises(ValueError):
+            self.calc.log10(-1)
+
+    def test_exp(self):
+        self.assertAlmostEqual(self.calc.exp(0), 1)
+        self.assertAlmostEqual(self.calc.exp(1), math.e)
+        self.assertAlmostEqual(self.math_class.exp(-1), 1/math.e)
+        self.assertAlmostEqual(self.math_class.exp(2), math.e**2)
 
 if __name__ == '__main__':
     unittest.main()
